ec7052e46501d5e19070db95b8eb2aeb5db49a6f
Tverksaac <artemosha12@gmail.com>
2025-08-29 14:50:52 +0300

    plot1

--- src/server/module_scripts/placement_system/plot.ts
+++ src/server/module_scripts/placement_system/plot.ts
@@ -2,7 +2,7 @@ export interface PlotParameters {
 	Size?: Vector2;
 	Position?: Vector3;
 	//OR
-	Reference: Part;
+	Reference?: Part;
 }
 function apply_texture(to: Part) {
 	const texture = new Instance("Texture");
@@ -17,10 +17,29 @@ function apply_texture(to: Part) {
 
 function create_from_reference(reference: Part) {
 	apply_texture(reference);
+
+	return reference;
 }
 
-function create_from_vector2(size: Vector2) {}
+function create_from_vector2(size: Vector2, pos: Vector3) {
+	const plot = new Instance("Part");
+
+	plot.Size = new Vector3(size.X, 1, size.Y);
+	plot.Position = pos;
+
+	return plot;
+}
 
 export class Plot {
-	constructor(Parameteres: PlotParameters) {}
+	constructor(Parameters: PlotParameters) {
+		let plot = undefined;
+		if (Parameters.Reference) {
+			plot = create_from_reference(Parameters.Reference);
+		} else if (Parameters.Position && Parameters.Size) {
+			plot = create_from_vector2(Parameters.Size, Parameters.Position);
+		} else {
+			throw error("Plot Parameteres was wrongly defined!", 1);
+		}
+		return plot;
+	}
 }
